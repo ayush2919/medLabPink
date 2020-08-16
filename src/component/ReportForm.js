c57fd2b05faf2a3328307e1241bafdcb3e54b7e3
ayush <ayushsaxena723@gmail.com>
2020-08-17 01:33:32 +0530

    rearrangementForm

--- src/component/ReportForm.js
+++ src/component/ReportForm.js
@@ -22,23 +22,7 @@ class ReportForm extends Component {
     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Submit</Button>
     </form> 
       </div>
-     </div>
-  //   <Card className="packages">
-  //   <CardHeader style={{backgroundColor:"#2295bc"}}><h5 className="text-white">
-  //   <i class="fa fa-address-card" aria-hidden="true"></i>  E-Report</h5></CardHeader>
-  //  <CardBody>
-  //  <form>
-  //   <div className="form-group">
-  //     <label for="exampleInputEmail1">
-  //     <i class="fa fa-envelope" aria-hidden="true"></i> Email</label>
-  //     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email"/>
-  //     <small id="emailHelp" className="form-text text-muted">Enter Registered Email </small>
-  //   </div>
-  //   <Button type="submit" className="btn" outline color="info">
-  //   <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Submit</Button>
-  // </form> 
-  //    </CardBody>
-  //  </Card>     
+     </div>    
    );   
   }
 }    
